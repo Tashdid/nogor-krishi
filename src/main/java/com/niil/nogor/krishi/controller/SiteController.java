545ad77b0fffc4e2b1b9379efd5a1815fccadd63
niildu <niildu@yahoo.com>
2018-05-12 23:37:04 +0600

    Almost completed ponno page

--- src/main/java/com/niil/nogor/krishi/controller/SiteController.java
+++ src/main/java/com/niil/nogor/krishi/controller/SiteController.java
@@ -1,11 +1,17 @@
 package com.niil.nogor.krishi.controller;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.niil.nogor.krishi.repo.ProductTypeRepo;
+import com.niil.nogor.krishi.entity.*;
+import com.niil.nogor.krishi.repo.*;
 
 /**
  * @author Noor
@@ -16,8 +22,14 @@ import com.niil.nogor.krishi.repo.ProductTypeRepo;
 @Controller
 @RequestMapping
 public class SiteController {
-	@Autowired
-	ProductTypeRepo productTypeRepo;
+
+	private Comparator<Nursery> byNurseryType = Comparator.comparing(
+			parent -> parent.getType().getSequence()
+	);
+
+	@Autowired ProductTypeRepo productTypeRepo;
+	@Autowired ProductRepo productRepo;
+	@Autowired ProductPriceRepo productPriceRepo;
 
 	@RequestMapping
 	public String homePage(final ModelMap model) {
@@ -26,7 +38,33 @@ public class SiteController {
 	}
 
 	@RequestMapping("/ponno")
-	public String ponnoPage() {
+	public String ponnoPage(final ModelMap model) {
+		List<ProductType> types = productTypeRepo.findAllByOrderBySequenceAsc();
+		model.addAttribute("types", types);
+		products(null, model);
 		return "site/ponno";
 	}
+
+	@RequestMapping("/ponno/{type}")
+	public String products(@PathVariable ProductType type, final ModelMap model) {
+		model.addAttribute("type", type);
+		List<Product> list = type == null ? productRepo.findAll() : productRepo.findAllByTypeOrderBySequenceAsc(type);
+		model.addAttribute("products", list);
+		return "site/ponno :: products";
+	}
+
+	@RequestMapping("/ponno/product/{product}")
+	public String product(@PathVariable Product product, final ModelMap model) {
+		model.addAttribute("product", product);
+		List<ProductPrice> prices = productPriceRepo.findAllByProduct(product);
+		List<Nursery> nurseries = prices.stream().map(p -> p.getNursery()).distinct()
+				.sorted(byNurseryType.thenComparing(Nursery::getSequence))
+				.collect(Collectors.toList());
+		model.addAttribute("nurseries", nurseries);
+		model.addAttribute("areas", nurseries.stream().map(n -> n.getArea()).distinct()
+				.sorted(Comparator.comparing(Area::getSequence))
+				.collect(Collectors.toList()));
+		return "site/product";
+	}
+
 }
--- src/main/resources/static/js/nogorkrishi.js
+++ src/main/resources/static/js/nogorkrishi.js
@@ -99,15 +99,116 @@ $(document).ready(function() {
 	});
 
 	var rmmeco = $.cookie("clickme");
-	console.log(rmmeco);
 	if (rmmeco) {
-		console.log("I am at here");
 		$.removeCookie("clickme");
 		$("#" + rmmeco).trigger("click");
 	} else if ($(".clickonload").length > 0) {
 		$(".clickonload").trigger("click");
 	}
+
+	$("body").on("click", ".types", function() {
+		$("#pclp-container").load($(this).attr("href"));
+		$("#alltype").text($(this).text()).show();
+		$(".alltype").hide();
+		$("#paginationdiv").show();
+		$(".pagestoshow").show();
+		loadPagination();
+		return false;
+	}).on("click", ".products", function() {
+		var ob = $(this);
+		$("#pclp-container").load(ob.attr("href"));
+		$("#typetext").attr("href", ob.data("typehref")).text(ob.data("typetext"));
+		$("#producttext").text($(".productname", ob).text());
+		$("#singleproduct").show();
+		$(".singleproduct").hide();
+		$("#paginationdiv").hide();
+		$(".pagestoshow").hide();
+		return false;
+	}).on("click", "#areaselection", function() {
+		var ar = $(this);
+		$(".nurseries").show();
+		if (ar.val() !== "")
+			$(".nurseries").not("." + ar.val()).hide();
+		return false;
+	}).on("change", ".pg", function() {
+		var ob = $(this);
+		$("." + ob.attr("id")).val(ob.val());
+		loadPagination();
+	}).on("click", ".arrows", function() {
+		var cp = parseInt($(".pagination > .pages.disabled").data("page"));
+		showPage((cp + ($(this).is(".leftarrow") ? -1 : 1)));
+		return false;
+	});
+	loadPagination();
 });
+
+function showPage(pageno) {
+	if ($("[data-page='" + pageno + "']").length === 0) return false;
+
+	var total = parseInt($("#totalproducts").val());
+	var itemsToShow = parseInt($("#pg1").val());
+	var pages = parseInt(total / itemsToShow);
+	if (total % itemsToShow > 0)
+		pages += 1;
+
+	var strt = (itemsToShow * (pageno - 1)) + 1;
+	var nd = itemsToShow * pageno;
+
+	$(".ponnoitems").hide();
+
+	for (var i = strt; i <= nd; i++) {
+		var itm = $("[data-index='" + i + "']");
+		if (itm.length > 0)
+			itm.show();
+	}
+
+	var dsbcls = "disabled";
+	var lft = $(".pagination > .left");
+	lft.removeClass(dsbcls);
+	if (pageno < 3 || pages < 4)
+		lft.addClass(dsbcls);
+
+	var rgt = $(".pagination > .right");
+	rgt.removeClass(dsbcls);
+	if ((pages - pageno) < 3)
+		rgt.addClass(dsbcls);
+
+	$(".pagination > .pages").removeClass(dsbcls).hide();
+	$('[data-page="' + pageno + '"]').addClass(dsbcls).show();
+	if (pageno > 1) {
+		$('[data-page="' + (pageno - 1) + '"]').show();
+	} else if ((pageno + 2) <= pages) {
+		$('[data-page="' + (pageno + 2) + '"]').show();
+	}
+	if (pageno < pages) {
+		$('[data-page="' + (pageno + 1) + '"]').show();
+	} else if ((pageno - 2) > 0) {
+		$('[data-page="' + (pageno - 2) + '"]').show();
+	}
+	return false;
+}
+
+function loadPagination() {
+	var total = parseInt($("#totalproducts").val());
+	var itemsToShow = parseInt($("#pg1").val());
+	var pages = parseInt(total / itemsToShow);
+	if (total % itemsToShow > 0)
+		pages += 1;
+
+	var pgdiv = $("#paginationdiv");
+	$(".pages", pgdiv).remove();
+	pgdiv.hide();
+	if (pages > 1) {
+		var item = $(".right", pgdiv);
+		for (var i = 1; i <= pages; i++) {
+			$('<li class="page-item pages" data-page="' + i + '"><a class="page-link" onClick="showPage(' + i + '); return false;" href="#">' + i + '</a></li>').insertBefore(item);
+		}
+		pgdiv.show();
+		showPage(1);
+	} else {
+		$(".ponnoitems").show();
+	}
+}
 
 function showImage(input) {
 	var imgid = $($(input).parent().find(".imgtoshow")[0]);
