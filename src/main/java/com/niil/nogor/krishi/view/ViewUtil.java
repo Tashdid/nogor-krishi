78417a737b67042c124759e207b37f48a8f27201
niildu <niildu@yahoo.com>
2018-09-05 12:17:20 +0600

    Fixed menu generation issue

--- src/main/java/com/niil/nogor/krishi/view/ViewUtil.java
+++ src/main/java/com/niil/nogor/krishi/view/ViewUtil.java
@@ -1,6 +1,12 @@
 package com.niil.nogor.krishi.view;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Component;
+
+import com.niil.nogor.krishi.entity.Menu;
+import com.niil.nogor.krishi.entity.Page;
 
 /**
  * @author Noor
@@ -13,5 +19,13 @@ public class ViewUtil {
 
 	public String titleToPath(String title) {
 		return title.replaceAll("\\s+", "_");
+	}
+
+	public List<Page> getPages(Map<Menu, List<Page>> pagesMap, Menu menu) {
+		return pagesMap.get(menu);
+	}
+
+	public String genUrl(List<Page> pages) {
+		return pages.isEmpty() ? "#" : ("/pages/" + pages.get(0).getId() + "/" + titleToPath(pages.get(0).getTitle()));
 	}
 }
